54256b79fb7dc90911235ac7736ede54886e43a8
DJHyun <djestiny4444@naver.com>
2019-11-13 16:03:44 +0900

    2019-11-13

--- BOJ/Python/17845_수강 과목.py
+++ BOJ/Python/17845_수강 과목.py
@@ -7,24 +7,24 @@
 '''
 import sys
 
+def solution(pos, cap):
+    print(pos,cap)
+    if pos == k:
+        return
+    ret = memo[pos][cap]
+    if ret != -1:
+        print(ret)
+    if sub[pos][1] <= cap:
+        ret = solution(pos+1,cap-sub[pos][0])+sub[pos][0]
+    ret = max(ret,solution(pos+1,cap))
+    memo[pos][cap] = ret
+    print(ret)
+    return memo[pos][cap]
+
 n, k = map(int, sys.stdin.readline().split())
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(k)]
-memo = [[0] * k for _ in range(k)]
-result = 0
-for i in arr:
-    print(i)
-print()
+memo = [[-1]*(n+1) for _ in range(k)]
 for i in memo:
     print(i)
-print()
-for i in range(k):
-    for j in range(k):
-        if i == j:
-            continue
+sub = [list(map(int,sys.stdin.readline().split())) for _ in range(k)]
 
-        elif arr[i][1] + arr[j][1] <= n:
-            memo[i][j] = arr[i][0] + arr[j][0]
-
-for i in memo:
-    print(i)
-print(result)
+solution(0,n)
--- BOJ/Python/9019_DSLR.py
+++ BOJ/Python/9019_DSLR.py
@@ -1,2 +1,60 @@
 # baekjoon source = "https://www.acmicpc.net/problem/9019"
 
+import sys
+from collections import deque
+
+def solution(x):
+    global b
+
+    q = deque([[x, []]])
+    visited[x] = 1
+    while q:
+        t = q.popleft()
+        number = t[0]
+        for i in range(4):
+            if i == 0:
+                new_number = (number * 2) % 10000
+                if new_number == b:
+                    return t[1] + ['D']
+                if not visited[new_number]:
+                    visited[new_number] = 1
+                    q.append([new_number, t[1]+['D']])
+            elif i == 1:
+                if number == 0:
+                    new_number = 9999
+                else:
+                    new_number = number - 1
+                if new_number == b:
+                    return t[1] + ['S']
+                if not visited[new_number]:
+                    visited[new_number] = 1
+                    q.append([new_number, t[1] + ['S']])
+            elif i == 2:
+                n = str(number)
+                zero = 4-len(n)
+                n = '0'*zero+n
+                new_number = n[1]+n[2]+n[3]+n[0]
+                new_number = int(new_number)
+                if new_number == b:
+                    return t[1] +['L']
+                if not visited[new_number]:
+                    visited[new_number] = 1
+                    q.append([new_number,t[1]+['L']])
+            else:
+                n = str(number)
+                zero = 4-len(n)
+                n = '0'*zero+n
+                new_number = n[3]+n[0]+n[1]+n[2]
+                new_number = int(new_number)
+                if new_number == b:
+                    return t[1]+['R']
+                if not visited[new_number]:
+                    visited[new_number] = 1
+                    q.append([new_number, t[1]+['R']])
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    a, b = map(int, sys.stdin.readline().split())
+    visited = [0] * 10001
+
+    print(''.join(solution(a)))
