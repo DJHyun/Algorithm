99b44a287f4271d508cde30051a3ef782dde8fe7
DJHyun <djestiny4444@naver.com>
2019-12-03 17:17:21 +0900

    2019-12-03

--- BOJ/Python/2231_분해합.py
+++ BOJ/Python/2231_분해합.py
@@ -1,11 +1,18 @@
 # baekjoon source = "https://www.acmicpc.net/problem/2231"
 import sys
-n = sys.stdin.readline().strip()
-len_ = len(n)
-for i in range(10 ** (len_-1), 10 ** len_ - 1):
-    st = str(i)
-    for j in range(len_):
-        i += int(st[j])
-    if i == int(n):
-        print(int(st))
+
+n = int(sys.stdin.readline().strip())
+
+st = '1'
+while int(st) < n:
+    len_ = len(st)
+    sum_ = 0
+    for i in range(len_):
+        sum_ += int(st[i])
+    new_st = int(st)+sum_
+    if new_st == n:
+        print(st)
         break
+    st = str(int(st)+1)
+else:
+    print(0)
--- BOJ/Python/2869_달팽이는 올라가고 싶다.py
+++ BOJ/Python/2869_달팽이는 올라가고 싶다.py
@@ -1,24 +1,9 @@
 # baekjoon source = "https://www.acmicpc.net/problem/2869"
 
 import sys
-
+import math
 a, b, v = map(int, sys.stdin.readline().split())
-left = 1
-right = v
-mid = (right+left)//2
-
-while True:
-
-    print(left,mid,right)
-    if (a-b)*mid+a > v:
-        right = mid -1
-    else:
-        left = mid + 1
-
-    if left >= right:
-        print(mid)
-        break
-
-    mid = (right+left)//2
-
-
+if a == b:
+    print(1)
+else:
+    print(math.ceil((v - a) / (a - b)) + 1)
